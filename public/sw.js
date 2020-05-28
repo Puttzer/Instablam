61797c92e24a7f6ef8ca071971bb06751cc8ceaf
Patrick Carragher <carragher@live.se>
2020-05-28 16:43:12 +0200

    Sw changed

--- public/sw.js
+++ public/sw.js
@@ -1,72 +1,70 @@
-let cacheName = 'InstablamTime';
-
-var contentToCache = [
-  '/offline.html',
-];
-
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Installing');
-
-    self.skipWaiting()
-
-    e.waitUntil(
-        caches.open(cacheName).then((cache) => {
-            console.log('[Service Worker] Caching static files');
-            return cache.addAll(contentToCache);
-        })
-    );
-});
-
-self.addEventListener('activate', (e) => {
-    console.log('[Service Worker] Activated');
-
-    // Deletes old cache
-    const cacheWhitelist = [cacheName];
-
-    e.waitUntil(
-      caches.keys().then(nameOfCaches => {
-        return Promise.all(
-          nameOfCaches.map(title => {
-            if(cacheWhitelist.indexOf(title) === -1) {
-              return caches.delete(title)
-            }
-          })
-        )
-      })
-    )
-});
-
-self.addEventListener('fetch', event => {
-  // console.log('Fetch event for ', event.request.url);
-  event.respondWith(
-    caches.match(event.request)
-    .then(response => {
-      // Returns cached site
-      if (response) {
-        // console.log('Found ', event.request.url, ' in cache');
-        return response;
-      }
-
-      //Fetch new
-      // console.log('Network request for ', event.request.url);
-      return fetch(event.request)
-
-
-      // Saves the new fetched site in cache
-      .then(response => {
-        return caches.open(cacheName).then(cache => {
-          
-          cache.put(event.request.url, response.clone());
-          return response;
-        });
-      });
-
-      // Returns custom offline.html if page is not cached
-    }).catch(error => {
-
-      console.log('[Service worker] ', error)
-      return caches.match('offline.html')
-
+const staticCacheName = "static-files";
+const assets = ["/", "/index.html", "/offline.html"];
+// Installing SW
+self.addEventListener("install", (evt) => {
+  console.log(self);
+  self.skipWaiting();
+  console.log(evt);
+  console.log("SW installed at: ", new Date().toLocaleTimeString());
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      cache.addAll(assets);
     })
   );
 });
+// activating SW
+self.addEventListener("activate", (event) => {
+  self.skipWaiting();
+  console.log("SW activated at: ", new Date().toLocaleTimeString());
+});
+// Fetching SW
+self.addEventListener("fetch", (evt) => {
+  console.log(evt.request.url);
+  if (!navigator.onLine) {
+    evt.respondWith(
+      caches.match(evt.request).then((response) => {
+        console.log("RESPONSE: ", response);
+        if (response) {
+          return response;
+        } else {
+          return caches.match(new Request("offline.html"));
+        }
+      })
+    );
+  } else {
+    console.log("IM ALIVE!");
+    if (evt.request.method === "GET") {
+      return updateCache(evt.request);
+    } else {
+      return fetch(evt.request);
+    }
+  }
+});
+
+//Listen to pushnotices
+self.addEventListener("push", (event) => {
+  console.log("push");
+  if (event.data) {
+    createNotification(event.data.text());
+  }
+});
+
+// Creat a notification with notification API
+const createNotification = (text) => {
+  self.registration.showNotification("You got a push!", {
+    body: text,
+    icon: "./images/icons/icon192x192.png",
+  });
+};
+
+function updateCache(request) {
+  return fetch(request).then((response) => {
+    if (response) {
+      return caches.open(staticCacheName).then((cache) => {
+        return cache.put(request, response.clone()).then(() => {
+          return response;
+        });
+      });
+    }
+  });
+}
